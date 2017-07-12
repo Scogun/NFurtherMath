e485acce3d1664bd8a95e897e72a125b36b4e5e2
Scogun <SAntonov@UCASoft.com>
2017-07-12 13:32:53 +0200

    Add Quaternion class

---
+++ FurtherMath/Quaternion.cs
@@ -0,0 +1,265 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace FurtherMath
+{
+    /// <summary>
+    /// The quaternion.
+    /// </summary>
+    public class Quaternion
+    {
+        /// <summary>
+        /// The real.
+        /// </summary>
+        private readonly double real;
+
+        /// <summary>
+        /// The i imaginary.
+        /// </summary>
+        private readonly double iImaginary;
+
+        /// <summary>
+        /// The j imaginary.
+        /// </summary>
+        private readonly double jImaginary;
+
+        /// <summary>
+        /// The k imaginary.
+        /// </summary>
+        private readonly double kImaginary;
+
+        /// <summary>
+        /// Initializes a new instance of the <see cref="Quaternion"/> class.
+        /// </summary>
+        /// <param name="real">
+        /// The real.
+        /// </param>
+        /// <param name="iImaginary">
+        /// The i imaginary.
+        /// </param>
+        /// <param name="jImaginary">
+        /// The j imaginary.
+        /// </param>
+        /// <param name="kImaginary">
+        /// The k imaginary.
+        /// </param>
+        public Quaternion(double real, double iImaginary, double jImaginary, double kImaginary)
+        {
+            this.real = real;
+            this.iImaginary = iImaginary;
+            this.jImaginary = jImaginary;
+            this.kImaginary = kImaginary;
+        }
+
+        /// <summary>
+        /// The precision.
+        /// </summary>
+        internal static double Precision => 1 * Math.Pow(10, -15);
+
+        /// <summary>
+        /// The *.
+        /// </summary>
+        /// <param name="firstQuaternion">
+        /// The first quaternion.
+        /// </param>
+        /// <param name="secondQuaternion">
+        /// The second quaternion.
+        /// </param>
+        /// <returns>
+        /// The new quaternion.
+        /// </returns>
+        public static Quaternion operator *(Quaternion firstQuaternion, Quaternion secondQuaternion)
+        {
+            double newReal = firstQuaternion.real * secondQuaternion.real
+                             - firstQuaternion.iImaginary * secondQuaternion.iImaginary
+                             - firstQuaternion.jImaginary * secondQuaternion.jImaginary
+                             - firstQuaternion.kImaginary * secondQuaternion.kImaginary;
+            double newIImaginary = firstQuaternion.real * secondQuaternion.iImaginary
+                                   + firstQuaternion.iImaginary * secondQuaternion.real
+                                   + firstQuaternion.jImaginary * secondQuaternion.kImaginary
+                                   - firstQuaternion.kImaginary * secondQuaternion.jImaginary;
+            double newJImaginary = firstQuaternion.real * secondQuaternion.jImaginary
+                                   - firstQuaternion.iImaginary * secondQuaternion.kImaginary
+                                   + firstQuaternion.jImaginary * secondQuaternion.real
+                                   + firstQuaternion.kImaginary * secondQuaternion.iImaginary;
+            double newKImaginary = firstQuaternion.real * secondQuaternion.kImaginary
+                                   + firstQuaternion.iImaginary * secondQuaternion.jImaginary
+                                   - firstQuaternion.jImaginary * secondQuaternion.iImaginary
+                                   + firstQuaternion.kImaginary * secondQuaternion.real;
+            return new Quaternion(newReal, newIImaginary, newJImaginary, newKImaginary);
+        }
+
+        /// <summary>
+        /// The to matrix.
+        /// </summary>
+        /// <param name="quaternion">
+        /// The quaternion.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Matrix"/>.
+        /// </returns>
+        public static Matrix ToMatrix(Quaternion quaternion)
+        {
+            return new Matrix(new List<double[]>
+                                  {
+                                      new[]
+                                          {
+                                              quaternion.real, -quaternion.iImaginary, -quaternion.jImaginary, -quaternion.kImaginary
+                                          },
+                                      new[]
+                                          {
+                                              quaternion.iImaginary, quaternion.real, -quaternion.kImaginary, quaternion.jImaginary
+                                          },
+                                      new[]
+                                          {
+                                              quaternion.jImaginary, quaternion.kImaginary, quaternion.real, -quaternion.iImaginary
+                                          },
+                                      new[]
+                                          {
+                                              quaternion.kImaginary, -quaternion.jImaginary, quaternion.iImaginary, quaternion.real
+                                          }
+                                  });
+        }
+
+        /// <summary>
+        /// The norm.
+        /// </summary>
+        /// <param name="quaternion">
+        /// The quaternion.
+        /// </param>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public static double Abs(Quaternion quaternion)
+        {
+            return Math.Sqrt(
+                Math.Pow(quaternion.real, 2) + Math.Pow(quaternion.iImaginary, 2) + Math.Pow(quaternion.jImaginary, 2)
+                + Math.Pow(quaternion.kImaginary, 2));
+        }
+
+        /// <summary>
+        /// The conjugate.
+        /// </summary>
+        /// <param name="quaternion">
+        /// The quaternion.
+        /// </param>
+        /// <returns>
+        /// The <see cref="Quaternion"/>.
+        /// </returns>
+        public static Quaternion Conjugate(Quaternion quaternion)
+        {
+            return new Quaternion(
+                quaternion.real,
+                -quaternion.iImaginary,
+                -quaternion.jImaginary,
+                -quaternion.kImaginary);
+        }
+
+        /// <summary>
+        /// The to matrix.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Matrix"/>.
+        /// </returns>
+        public Matrix ToMatrix()
+        {
+            return ToMatrix(this);
+        }
+
+        /// <summary>
+        /// The norm.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="double"/>.
+        /// </returns>
+        public double Abs()
+        {
+            return Abs(this);
+        }
+
+        /// <summary>
+        /// The conjugate.
+        /// </summary>
+        /// <returns>
+        /// The <see cref="Quaternion"/>.
+        /// </returns>
+        public Quaternion Conjugate()
+        {
+            return Conjugate(this);
+        }
+
+        /// <summary>Determines whether the specified object is equal to the current object.</summary>
+        /// <returns>true if the specified object  is equal to the current object; otherwise, false.</returns>
+        /// <param name="obj">The object to compare with the current object. </param>
+        public override bool Equals(object obj)
+        {
+            Quaternion quaternion = obj as Quaternion;
+            if (quaternion == null)
+            {
+                return false;
+            }
+
+            return Math.Abs(this.real - quaternion.real) < Precision
+                   && Math.Abs(this.iImaginary - quaternion.iImaginary) < Precision
+                   && Math.Abs(this.jImaginary - quaternion.jImaginary) < Precision
+                   && Math.Abs(this.kImaginary - quaternion.kImaginary) < Precision;
+        }
+
+        /// <summary>Serves as the default hash function. </summary>
+        /// <returns>A hash code for the current object.</returns>
+        public override int GetHashCode()
+        {
+            unchecked
+            {
+                var hashCode = this.real.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.iImaginary.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.jImaginary.GetHashCode();
+                hashCode = (hashCode * 397) ^ this.kImaginary.GetHashCode();
+                return hashCode;
+            }
+        }
+
+        /// <summary>Returns a string that represents the current object.</summary>
+        /// <returns>A string that represents the current object.</returns>
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            if (Math.Abs(real) > Precision)
+            {
+                builder.Append(real);
+            }
+
+            AddImaginary(builder, iImaginary, 'i');
+            AddImaginary(builder, jImaginary, 'j');
+            AddImaginary(builder, kImaginary, 'k');
+
+            return builder.ToString();
+        }
+
+        /// <summary>
+        /// The add imaginary.
+        /// </summary>
+        /// <param name="builder">
+        /// The builder.
+        /// </param>
+        /// <param name="imaginary">
+        /// The imaginary.
+        /// </param>
+        /// <param name="imaginaryName">
+        /// The imaginary name.
+        /// </param>
+        private void AddImaginary(StringBuilder builder, double imaginary, char imaginaryName)
+        {
+            if (Math.Abs(imaginary) > Precision)
+            {
+                if (builder.Length > 0 && imaginary > 0)
+                {
+                    builder.Append("+");
+                }
+
+                builder.Append($"{imaginary}{imaginaryName}");
+            }
+        }
+    }
+}
---
+++ FurtherMathTests/QuaternionTests.cs
@@ -0,0 +1,87 @@
+﻿using FurtherMath;
+
+using Xunit;
+
+namespace FurtherMathTests
+{
+    /// <summary>
+    /// The quaternion tests.
+    /// </summary>
+    public class QuaternionTests
+    {
+        /// <summary>
+        /// The multiplication.
+        /// </summary>
+        /// <param name="firstReal">
+        /// The first Real.
+        /// </param>
+        /// <param name="firstIImaginary">
+        /// The first I Imaginary.
+        /// </param>
+        /// <param name="firstJImaginary">
+        /// The first J Imaginary.
+        /// </param>
+        /// <param name="firstKImaginary">
+        /// The first K Imaginary.
+        /// </param>
+        /// <param name="secondReal">
+        /// The second Real.
+        /// </param>
+        /// <param name="secondIImaginary">
+        /// The second I Imaginary.
+        /// </param>
+        /// <param name="secondJImaginary">
+        /// The second J Imaginary.
+        /// </param>
+        /// <param name="secondKImaginary">
+        /// The second K Imaginary.
+        /// </param>
+        /// <param name="result">
+        /// The result.
+        /// </param>
+        [Theory]
+        [InlineData(2, 3, 4, 5, 1, 1, 1, 1, "-10+4i+8j+6k")]
+        [InlineData(2, 3, 4, 5, 6, 7, 8, 9, "-86+28i+48j+44k")]
+        [InlineData(2, 1, 4, 0, 6, 7, 0, 9, "5+56i+15j-10k")]
+        public void Multiplication(double firstReal, double firstIImaginary, double firstJImaginary, double firstKImaginary, double secondReal, double secondIImaginary, double secondJImaginary, double secondKImaginary, string result)
+        {
+            Quaternion firstQuaternion = new Quaternion(firstReal, firstIImaginary, firstJImaginary, firstKImaginary);
+            Quaternion secondQuaternion = new Quaternion(secondReal, secondIImaginary, secondJImaginary, secondKImaginary);
+            Quaternion thirdQuaternion = firstQuaternion * secondQuaternion;
+            Assert.Equal(result, thirdQuaternion.ToString());
+        }
+
+        /// <summary>
+        /// The to matrix.
+        /// </summary>
+        [Fact]
+        public void ToMatrix()
+        {
+            Quaternion quaternion = new Quaternion(1, 2, 3, 4);
+            Assert.Equal("1 -2 -3 -4\r\n2 1 -4 3\r\n3 4 1 -2\r\n4 -3 2 1", quaternion.ToMatrix().ToString());
+        }
+
+        /// <summary>
+        /// The norm.
+        /// </summary>
+        [Fact]
+        public void Abs()
+        {
+            Quaternion quaternion = new Quaternion(2, 2, 2, 2);
+            Assert.Equal(4, quaternion.Abs());
+        }
+
+        /// <summary>
+        /// The operations.
+        /// </summary>
+        [Fact]
+        public void Operations()
+        {
+            Quaternion firstQuaternion = new Quaternion(2, 3, 4, 5);
+            Quaternion secondQuaternion = new Quaternion(6, 7, 8, 9);
+            Assert.Equal(
+                (firstQuaternion * secondQuaternion).Conjugate(),
+                secondQuaternion.Conjugate() * firstQuaternion.Conjugate());
+        }
+    }
+}
