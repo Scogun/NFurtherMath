52f47a55f39537b6555cad3e00fcb508ee0eb655
Scogun <SAntonov@UCASoft.com>
2021-12-28 14:59:18 +0100

    Add Quadratic Equation Solution

---
+++ FurtherMath.Calculations/QuadraticEquation.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Text;
+
+namespace FurtherMath.Calculations
+{
+    public class QuadraticEquation
+    {
+        private readonly double a;
+
+        private readonly double b;
+
+        private readonly double c;
+
+        public QuadraticEquation(double a, double b, double c)
+        {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+        public double Discriminant => Math.Pow(b, 2) - 4 * a * c;
+
+        public QuadraticEquationRoots Solution()
+        {
+            if (Discriminant >= 0)
+            {
+                return new QuadraticEquationRoots((-b + Math.Sqrt(Discriminant)) / (2 * a), (-b - Math.Sqrt(Discriminant)) / (2 * a));
+            }
+
+            var imaginary = new Complex(0, Math.Sqrt(Math.Abs(Discriminant)));
+            return new QuadraticEquationRoots((-b + imaginary) / (2 * a), (-b - imaginary) / (2 * a));
+        }
+
+        public override string ToString()
+        {
+            StringBuilder builder = new StringBuilder();
+            if (a < 0)
+            {
+                builder.Append('-');
+            }
+
+            if (a != 0 && Math.Abs(a) != 1)
+            {
+                builder.Append(a);
+            }
+
+            if (a != 0)
+            {
+                builder.Append("x^2");
+            }
+
+            if (b > 0)
+            {
+                builder.Append('+');
+            }
+            else if (b == -1)
+            {
+                builder.Append('-');
+            }
+
+            if (b != 0 && Math.Abs(b) != 1)
+            {
+                builder.Append(b);
+            }
+
+            if (b != 0)
+            {
+                builder.Append("x");
+            }
+
+            if (c > 0)
+            {
+                builder.Append("+");
+            }
+
+            if (c != 0)
+            {
+                builder.Append(c);
+            }
+
+            builder.Append("=0");
+
+            return builder.ToString();
+        }
+    }
+}
---
+++ FurtherMath.Calculations/QuadraticEquationRoots.cs
@@ -0,0 +1,55 @@
+﻿using System;
+
+namespace FurtherMath.Calculations
+{
+    public class QuadraticEquationRoots
+    {
+        private readonly double? firstDoubleRoot;
+
+        private readonly double? secondDoubleRoot;
+
+        private readonly Complex firstComplexRoot;
+
+        private readonly Complex secondComplexRoot;
+
+        public QuadraticEquationRoots(double firstRoot, double secondRoot)
+        {
+            firstDoubleRoot = firstRoot;
+            secondDoubleRoot = secondRoot;
+        }
+
+        public QuadraticEquationRoots(Complex firstRoot, Complex secondRoot)
+        {
+            firstComplexRoot = firstRoot;
+            secondComplexRoot = secondRoot;
+        }
+
+        public bool IsRealRoots => firstDoubleRoot.HasValue;
+
+        public (double FirstRoot, double SecondRoot) RealRoots
+        {
+            get
+            {
+                if (IsRealRoots)
+                {
+                    return (firstDoubleRoot.Value, secondDoubleRoot.Value);
+                }
+
+                throw new ArgumentException("Solution is a complex one.");
+            }
+        }
+
+        public (Complex FirstRoot, Complex SecondRoot) ComplexRoots
+        {
+            get
+            {
+                if (!IsRealRoots)
+                {
+                    return (firstComplexRoot, secondComplexRoot);
+                }
+
+                throw new ArgumentException("Solution is not a complex one.");
+            }
+        }
+    }
+}
