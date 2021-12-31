0790cd7eb5062a237febf98b900227ec860afc6d
Scogun <SAntonov@UCASoft.com>
2021-12-31 11:54:57 +0100

    Use standard Complex implementation
    Plus add extentions

--- FurtherMath.Calculations.Tests/QuadraticEquationTests.cs
+++ FurtherMath.Calculations.Tests/QuadraticEquationTests.cs
@@ -1,4 +1,5 @@
 using System;
+using System.Numerics;
 using FluentAssertions;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 
@@ -11,6 +12,7 @@ namespace FurtherMath.Calculations.Tests
         [DataRow(1, 0, -49, "x^2-49=0", 7, -7)]
         [DataRow(1, -8, 12, "x^2-8x+12=0", 6, 2)]
         [DataRow(-1, -2, 15, "-x^2-2x+15=0", -5, 3)]
+        [DataRow(1, 2, -15, "x^2+2x-15=0", 3, -5)]
         [DataRow(1, -6, 9, "x^2-6x+9=0", 3, 3)]
         [DataRow(1, -1, 0, "x^2-x=0", 1, 0)]
         public void RealQuadraticEquation(double a, double b, double c, string stringForm, double firstRoot, double secondRoot)
